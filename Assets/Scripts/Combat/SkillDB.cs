7855100fe19417d888108f7deb02af8c31b5f739
leesh8258 <102220481+leesh8258@users.noreply.github.com>
2024-07-24 02:20:07 +0900

    CombatSystem ver.1.04
    - 기획자가 필요한 스킬 변수 추가
    - 공격 횟수에 따라 데미지함수 발동되도록 수정
    - 버프 디버프 리스트로 변경하여 스킬 하나에 여러개의 효과가 발동될 수 있도록 수정

--- Assets/Scripts/Combat/SkillDB.cs
+++ Assets/Scripts/Combat/SkillDB.cs
@@ -1,22 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static SkillSO;
 
 public class SkillDB
 {
     public string NAME { get; private set; }
+    public SkillType TYPE { get; private set; }
+    public int NUMOFATTACK { get; private set; }
     public int DAMAGE { get; private set; }
     public int DISTANCE { get; private set; }
-    public StatusEffetSO EFFECT { get; private set; }
+    public StatusEffetSO[] EFFECT { get; private set; }
     public int MOVE { get; private set; }
     public Sprite ICON { get; private set; }
     
     public int USES;
     public int COOLTIME;
 
-    public SkillDB(string name, int damage, int distance, StatusEffetSO effect, int move, Sprite icon, int uses, int cooltime)
+    public SkillDB(string name, SkillType skillType, int numOfAttack, int damage, int distance, StatusEffetSO[] effect, int move, Sprite icon, int uses, int cooltime)
     {
         NAME = name;
+        TYPE = skillType;
+        NUMOFATTACK = numOfAttack;
         DAMAGE = damage;
         DISTANCE = distance;
         EFFECT = effect;
